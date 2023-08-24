604075bb5529f7c3ff164226949741935741f854
Management & Science University Malaysia <ahmadamry194@gmail.com>
2023-08-24 17:36:07 +0800

    updated
    updated

--- mobile/javascript/bookmark_config.js
+++ mobile/javascript/bookmark_config.js
@@ -1,1 +1,1 @@
-﻿var ols=[{caption:"wahyu-birthday-2023",page:"1",url:"",level:"1",children:[]},{caption:"happy-birthday-wahyu",page:"2",url:"",level:"1",children:[]},{caption:"wahyu-birthday-2023",page:"3",url:"",level:"1",children:[]}];
+﻿var ols=[{caption:"wish-latest-2023",page:"1",url:"",level:"1",children:[]},{caption:"happy-birthday-wahyu",page:"2",url:"",level:"1",children:[]},{caption:"wish-latest-2023",page:"3",url:"",level:"1",children:[]},{caption:"wish-latest-2023",page:"4",url:"",level:"1",children:[]}];
